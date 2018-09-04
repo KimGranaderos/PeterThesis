875db4bda3120f554b4656e1829e6924edbfa9c1
KimGranaderos <xxnamelessxx090897@gmail.com>
2018-09-04 20:50:20 +0800

    added product list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,58 +1,180 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-data-iterator
-      :items="items"
-      :rows-per-page-items="rowsPerPageItems"
-      :pagination.sync="pagination"
-      content-tag="v-layout"
-      row
-      wrap
-    >
-      <v-flex
-        slot="item"
-        slot-scope="props"
-        xs12
-        sm6
-        md4
-        lg3
-      >
-        <v-card>
-          <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
-          <v-divider></v-divider>
-          <v-list dense>
-            <v-list-tile>
-              <v-list-tile-content>Calories:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Fat:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Carbs:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Protein:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Sodium:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Calcium:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Iron:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
-            </v-list-tile>
-          </v-list>
+  <v-content>
+    <!-- Clients -->
+    <v-layout row wrap>
+      <v-container fluid grid-list-md>
+        <h3>Clients</h3>
+        <v-data-iterator
+          :items="items"
+          :rows-per-page-items="rowsPerPageItems"
+          :pagination.sync="pagination"
+          content-tag="v-layout"
+          row
+          wrap
+        >
+          <v-flex
+            slot="item"
+            slot-scope="props"
+            xs12
+            sm6
+            md4
+            lg3
+          >
+            <v-card>
+              <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
+              <v-divider></v-divider>
+              <v-list dense>
+                <v-list-tile>
+                  <v-list-tile-content>Calories:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Fat:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Carbs:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Protein:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Sodium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Calcium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Iron:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </v-data-iterator>
+      </v-container>
+    </v-layout>
+
+    <!-- Products -->
+    <v-layout row wrap>
+      <v-container fluid grid-list-md>
+        <v-card class="accent">
+          <v-card-title>
+            <h2>Products</h2>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-iterator
+            :items="productItems"
+            :rows-per-page-items="rowsPerPageItems"
+            :pagination.sync="pagination"
+            :search="search"
+            content-tag="v-layout"
+            row
+            wrap
+          >
+            <v-flex
+              slot="item" slot-scope="props" xs12 sm6 md4 lg3>
+
+              <v-card>
+                <v-img
+                  :src="props.item.imageUrl"
+                  :key="props.item.id"
+                  height="300px"
+                ></v-img>
+
+                <v-card-title primary-title>
+                  <div>
+                    <h3 class="mb-0">{{ props.item.name}}</h3>
+                    <div>Code: {{ props.item.code}}</div>
+                    <div>Price: {{ props.item.price}}</div>
+                  </div>
+                </v-card-title>
+
+                <v-card-actions>
+                  <v-btn flat color="orange">Purchase</v-btn>
+                  <v-btn flat color="orange">BOBO DAMIEL</v-btn>
+                </v-card-actions>
+              </v-card>
+
+            </v-flex>
+            <v-alert slot="no-results" :value="true" color="error" icon="warning">
+              Your search for "{{ search }}" found no results.
+            </v-alert>
+          </v-data-iterator>
         </v-card>
-      </v-flex>
-    </v-data-iterator>
-  </v-container>
+      </v-container>
+    </v-layout>
+
+    <!-- Projects -->
+    <v-layout row wrap>
+      <v-container fluid grid-list-md>
+        <h3>Projects</h3>
+        <v-data-iterator
+          :items="items"
+          :rows-per-page-items="rowsPerPageProducts"
+          :pagination.sync="paginationProd"
+          content-tag="v-layout"
+          row
+          wrap
+        >
+          <v-flex
+            slot="item"
+            slot-scope="props"
+            xs12
+            sm6
+            md4
+            lg3
+          >
+            <v-card>
+              <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
+              <v-divider></v-divider>
+              <v-list dense>
+                <v-list-tile>
+                  <v-list-tile-content>Calories:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Fat:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Carbs:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Protein:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Sodium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Calcium:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile>
+                  <v-list-tile-content>Iron:</v-list-tile-content>
+                  <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-card>
+          </v-flex>
+        </v-data-iterator>
+      </v-container>
+    </v-layout>
+  </v-content>
 </template>
 
 <script>
@@ -172,6 +294,70 @@
           sodium: 54,
           calcium: '12%',
           iron: '6%'
+        }
+      ],
+
+      rowsPerPageProducts: [4, 8, 12],
+      paginationProd: {
+        rowsPerPageProd: 4
+      },
+      search: '',
+      productItems: [
+        {
+          id: '1',
+          name: 'ARDUINO UNO REV3',
+          code: 'A000066',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/500x375/f8876a31b63532bbba4e781c30024a0a/a/0/a000066_front_3.jpg',
+          price: '$22.00'
+        },
+        {
+          id: '3',
+          name: 'ARDUINO 101',
+          code: 'ABX00005',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/A/B/ABX00005_featured_2.jpg',
+          price: 'Not Available'
+        },
+        {
+          id: '4',
+          name: 'ARDUINO MICRO',
+          code: 'A000053',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/a/0/a000053_featured_2.jpg',
+          price: '$19.80'
+        },
+        {
+          id: '5',
+          name: 'ARDUINO MEGA 2560 REV3',
+          code: 'A000067',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/a/0/a000067_featured_1_.jpg',
+          price: '$38.50'
+        },
+        {
+          id: '6',
+          name: 'GENUINO ZERO',
+          code: 'GBX00003',
+          imageUrl: 'https://store-cdn.arduino.cc/uni/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/G/B/GBX00003_featured_2.jpg',
+          price: '€39.00'
+        },
+        {
+          id: '7',
+          name: 'ARDUINO DUE',
+          code: 'A000062',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/A/0/A000062_featured_2.jpg',
+          price: '$37.40'
+        },
+        {
+          id: '8',
+          name: 'ARDUINO MEGA ADK REV3',
+          code: 'A000069',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/A/0/A000069_featured_2.jpg',
+          price: 'RETIRED'
+        },
+        {
+          id: '2',
+          name: 'ARDUINO LEONARDO WITH HEADERS',
+          code: 'A000057',
+          imageUrl: 'https://store-cdn.arduino.cc/usa/catalog/product/cache/1/image/520x330/604a3538c15e081937dbfbd20aa60aad/A/0/A000057_featured_2.jpg',
+          price: '$19.80 '
         }
       ]
     })
